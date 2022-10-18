43e4e78953192bfba60f42fad985f36e1c43e899
LEMASSON Louis <galou2608@gmail.com>
2022-10-18 10:05:21 +0200

    Create Artotheque.php

---
+++ src/classes/Artotheque.php
@@ -0,0 +1,17 @@
+<?php
+
+class Artotheque {
+
+    private static $artotheques;
+
+    private $id;
+    private $city;
+    private $rooms;
+    private $artworks;
+    private $expositions;
+
+    public function __construct(string $city) {
+        
+    }
+
+}
