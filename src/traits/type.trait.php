13f3d85e87a3d1d2065848b2398ee6c7430aaddc
LEMASSON Louis <galou2608@gmail.com>
2022-10-18 11:12:26 +0200

    Update type.trait.php

--- src/traits/type.trait.php
+++ src/traits/type.trait.php
@@ -6,16 +6,46 @@ trait Type {
     protected string $name;
 
     function __construct(int $id, string $name) {
-        $this->id = $id;
-        $this->name = $name;
+        $this->setId($id);
+        $this->setName($name);
     }
 
+    /**
+     * Get the value of id.
+     *
+     * @return integer
+     */
     function getId(): int {
         return $this->id;
     }
 
+    /**
+     * Set the value of id.
+     *
+     * @param integer $id
+     * @return void
+     */
+    protected function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    /**
+     * Get the value of name.
+     *
+     * @return string
+     */
     function getName(): string {
         return $this->name;
+    }
+
+    /**
+     * Set the value of name.
+     *
+     * @param string $name
+     * @return void
+     */
+    protected function setName(string $name) {
+        $this->name = $name;
     }
 
 }
